fae269bfc1cf35f807185842bcb1ed5f7f0a37cc
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-05 21:35:51 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,26 +20,31 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
+  # Defined array in method then removed element from end of array using .pop
   array = [1, 2, 3, "arrays!"]
   array.pop
 end
 
 def remove_element_from_start_of_array(array)
+  # Defined array in method then removed first element using .shift
   array = ["wow", 2, 3, 4]
   array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
+  # Defined array in method then called on second element in array
   array = ["wow", "am", "shamwow"]
   array[1]
 end
 
 def retrieve_first_element_from_array(array)
+  # Defined array in method then called on first element in array
   array = ["wow", "shamwow", "wowzers"]
   array[0]
 end
 
 def retrieve_last_element_from_array(array)
+  # Defined array in method then called on last element in array
 array = ["wow", "shamwow", "arrays!"]
   array[2]
 end
